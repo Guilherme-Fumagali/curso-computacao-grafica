bf2a3fef0fbf2a13635247341e9d4f06f6cda095
gfumagali <guilhermefumarques@gmail.com>
2023-10-15 21:37:54 -0300

    build: Init tests

---
+++ Atividade_2/tests/Vec2Test.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+#include "../src/vec2.h"
+
+TEST(Vec2, Constructor) {
+    vec2 v1;
+    vec2 v2(1, 2);
+
+    EXPECT_EQ(v1.x(), 0);
+    EXPECT_EQ(v1.y(), 0);
+
+    EXPECT_EQ(v2.x(), 1);
+    EXPECT_EQ(v2.y(), 2);
+}

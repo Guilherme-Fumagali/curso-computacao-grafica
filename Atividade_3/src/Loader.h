cc738cdbd4ddbb76a5d1c58656e606e469b7af32
gfumagali <guilhermefumarques@gmail.com>
2023-10-24 23:06:22 -0300

    feat: reads uvs and normals too

--- Atividade_3/src/Loader.h
+++ Atividade_3/src/Loader.h
@@ -1,6 +1,7 @@
 #ifndef OBJREADER_H
 #define OBJREADER_H
 
+#include "vec2.h"
 #include "vec3.h"
 #include "Triangle.h"
 #include <vector>
@@ -10,26 +11,60 @@
 
 using namespace std;
 
+/**
+ * @brief Class for loading .obj files.
+ * @details The class contains a static method for loading .obj files.
+ * The method takes a file path as argument and returns a vector of vertices and a vector of triangles.
+ * The vertices are stored as {@see vec3} vectors and the triangles are stored as {@see Triangle} objects.
+ */
 class Loader {
 
 public:
-    static bool loadObj(const char *source, vector<vec3>* out_vertices, vector<Triangle>* out_triangles) {
+
+    /**
+     * @brief Loads a .obj file.
+     * @param source The path to the .obj file.
+     * @param out_vertices A pointer to a vector of vertices.
+     * @param out_triangles A pointer to a vector of triangles.
+     * @return True if the file was loaded successfully, false otherwise.
+     */
+    static bool loadObj(const char *source, vector<vec3>* out_vertices, vector<vec2>* out_uvs, vector<vec3>* out_normals, vector<Triangle>* out_triangles) {
+
+        // Try to open the file
         ifstream file(source);
         if(!file.is_open()){
             std::cerr << "Error: Could not open file " << source << std::endl;
             return false;
         }
 
+        // Read the file line by line
         string line;
         while(true) {
-            if (file.eof()) break;
+            if (file.eof()) break; // end of file
             getline(file, line);
 
             // vertex data
             if (line.substr(0, 2) == "v ") {
                 vec3 vertex;
+                // read the three doubles from the line and store them in the vec3
                 sscanf(line.c_str(), "v %lf %lf %lf\n", &vertex[0], &vertex[1], &vertex[2]);
-                out_vertices->push_back(vertex);
+                out_vertices->push_back(vertex); // add the vertex to the vector
+            }
+
+            // texture data
+            if (line.substr(0, 2) == "vt") {
+                vec2 uv;
+                // read the two doubles from the line and store them in the vec2
+                sscanf(line.c_str(), "vt %lf %lf\n", &uv[0], &uv[1]);
+                out_uvs->push_back(uv); // add the uv to the vector
+            }
+
+            // normal data
+            if (line.substr(0, 2) == "vn") {
+                vec3 normal;
+                // read the three doubles from the line and store them in the vec3
+                sscanf(line.c_str(), "vn %lf %lf %lf\n", &normal[0], &normal[1], &normal[2]);
+                out_normals->push_back(normal); // add the normal to the vector
             }
 
             // face data
@@ -40,18 +75,19 @@ public:
                                      &vertexIndex[1], &uvIndex[1], &normalIndex[1],
                                      &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
 
+                // check if the line was read correctly
                 if (matches != 9) {
                     std::cerr << "Error: Could not read face data from line " << line << std::endl;
                     return false;
                 }
 
-                vector<vec3> faces;
+                vector<vec3> faces; // vector for storing the faces
                 for (int i = 0; i < 3; i++)
                     faces.emplace_back(vertexIndex[i] - 1, uvIndex[i] - 1, normalIndex[i] - 1);
 
+                // add the triangle to the vector
                 out_triangles->emplace_back(faces[0], faces[1], faces[2]);
             }
-
         }
 
         file.close();
--- Atividade_3/src/main.cpp
+++ Atividade_3/src/main.cpp
@@ -11,11 +11,13 @@ int main(int argc, char **argv) {
     }
 
     vector<vec3> vertices;
+    vector<vec2> uvs; // texture coordinates
+    vector<vec3> normals;
     vector<Triangle> triangles;
 
     const char *file = argv[1];
 
-    bool res = Loader::loadObj(file, &vertices, &triangles);
+    bool res = Loader::loadObj(file, &vertices, &uvs, &normals, &triangles);
     if (!res) {
         std::cerr << "Error: Could not load obj file " << argv[1] << std::endl;
         return 1;
@@ -25,6 +27,18 @@ int main(int argc, char **argv) {
     for (int i = 0; i < vertices.size(); i++) {
         vec3 vertice = vertices[i];
         std::cout << i << "\t" << vertice.x() << " " << vertice.y() << " " << vertice.z() << std::endl;
+    }
+
+    std::cout << "UVs:" << std::endl;
+    for (int i = 0; i < uvs.size(); i++) {
+        vec2 uv = uvs[i];
+        std::cout << i << "\t" << uv.x() << " " << uv.y() << std::endl;
+    }
+
+    std::cout << "Normals:" << std::endl;
+    for (int i = 0; i < normals.size(); i++) {
+        vec3 normal = normals[i];
+        std::cout << i << "\t" << normal.x() << " " << normal.y() << " " << normal.z() << std::endl;
     }
 
     std::cout << "Triangles:" << std::endl;
